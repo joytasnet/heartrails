8cfafb1617b08107b4fc7ccb38a4681dd2a877c5
mjpurin <mjpurin@gmail.com>
2019-09-13 11:37:22 +0900

    TrainParser リファクタリング

--- src/model/TrainParser.java
+++ src/model/TrainParser.java
@@ -15,36 +15,41 @@ import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
 
 public class TrainParser {
+	private HttpURLConnection con;
+	private JsonReader jr;
+	private void readJson(String urlString) throws IOException {
+		URL url=new URL(urlString);
+		this.con=(HttpURLConnection)url.openConnection();
+		this.con.setRequestMethod("GET");
+		InputStream is=this.con.getInputStream();
+		InputStreamReader isr=new InputStreamReader(is,"UTF-8");
+		this.jr=new JsonReader(isr);
+	}
+	private void disconnect() throws IOException {
+		if(this.jr !=null) {
+			this.jr.close();
+		}
+		if(this.con !=null) {
+			this.con.disconnect();
+		}
+	}
 	public List<String> getRails(String urlString){
 		List<String> list=new ArrayList<>();
-		HttpURLConnection con=null;
-		JsonReader jr=null;
+
 		try {
-			URL url=new URL(urlString);
-			con=(HttpURLConnection)url.openConnection();
-			con.setRequestMethod("GET");
-			InputStream is=con.getInputStream();
-			InputStreamReader isr=new InputStreamReader(is,"UTF-8");
-			jr=new JsonReader(isr);
+			this.readJson(urlString);
 			JsonObject root=new Gson().fromJson(jr,JsonObject.class);
 			JsonArray lines=root.get("response").getAsJsonObject().get("line").getAsJsonArray();
 			for(JsonElement ele:lines) {
 				list.add(ele.getAsString());
 			}
 		} catch (IOException e) {
-			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}finally {
-			if(jr != null) {
-				try {
-					jr.close();
-				} catch (IOException e) {
-					// TODO 自動生成された catch ブロック
-					e.printStackTrace();
-				}
-			}
-			if(con !=null) {
-				con.disconnect();
+			try {
+				this.disconnect();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 		System.out.println(list.size()+list.get(0));
@@ -52,34 +57,21 @@ public class TrainParser {
 	}
 	public List<String> getStations(String urlString){
 		List<String> list=new ArrayList<>();
-		HttpURLConnection con=null;
-		JsonReader jr=null;
 		try {
-			URL url=new URL(urlString);
-			con=(HttpURLConnection)url.openConnection();
-			con.setRequestMethod("GET");
-			InputStream is=con.getInputStream();
-			InputStreamReader isr=new InputStreamReader(is,"UTF-8");
-			jr=new JsonReader(isr);
+			this.readJson(urlString);
 			JsonObject root=new Gson().fromJson(jr,JsonObject.class);
 			JsonArray stations=root.get("response").getAsJsonObject().get("station").getAsJsonArray();
 			for(JsonElement ele:stations) {
 				list.add(ele.getAsJsonObject().get("name").getAsString());
 			}
 		} catch (IOException e) {
-			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}finally {
-			if(jr != null) {
-				try {
-					jr.close();
-				} catch (IOException e) {
-					// TODO 自動生成された catch ブロック
-					e.printStackTrace();
-				}
-			}
-			if(con !=null) {
-				con.disconnect();
+			try {
+				this.disconnect();
+			} catch (IOException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
 			}
 		}
 		System.out.println(list.size()+list.get(0));
